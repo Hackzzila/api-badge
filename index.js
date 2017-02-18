8ffc1e44777c6529e23431e7916d6c1509711fbb
Zachary Vacura <zvacura@outlook.com>
2017-02-18 11:35:17 -0600

    dont redirect (causes caching issues)

--- index.js
+++ index.js
@@ -18,6 +18,16 @@ const green = Color({ h: 120, s: 65, l: 50 }).hex().substr(1);
 
 app.set('x-powered-by', false);
 
+function display(res, url) {
+  superagent.get(url)
+    .then((r) => {
+      res.header('content-type', 'image/svg+xml');
+      res.send(r.body);
+    }).catch(() => {
+      res.status(500).end();
+    });
+}
+
 app.get('/v1/ping', (req, res) => {
   res.status(200).end();
 });
@@ -28,7 +38,7 @@ app.get('/v1', (req, res) => {
   res.header('expires', date.toUTCString());
   res.header('cache-control', 'no-cache');
 
-  if (!req.query.url) return res.redirect(`https://img.shields.io/badge/api-no url param-${red}.svg`);
+  if (!req.query.url) return display(res, `https://img.shields.io/badge/api-no url param-${red}.svg`);
 
   superagent.get(req.query.url)
     .use(responseTime((r, time) => {
@@ -39,20 +49,20 @@ app.get('/v1', (req, res) => {
         const hue = 120 - (120 * ((time - stats.min) / (stats.max - stats.min)));
 
         if (isFinite(hue)) {
-          res.redirect(`https://img.shields.io/badge/api-${time.toFixed()}ms-${Color({ h: hue, s: 65, l: 50 }).hex().substr(1)}.svg`);
-        } else res.redirect(`https://img.shields.io/badge/api-${time.toFixed()}ms-${green}.svg`);
+          display(res, `https://img.shields.io/badge/api-${time.toFixed()}ms-${Color({ h: hue, s: 65, l: 50 }).hex().substr(1)}.svg`);
+        } else display(res, `https://img.shields.io/badge/api-${time.toFixed()}ms-${green}.svg`);
       }
     }))
     .catch((r) => {
-      if (res.timeout) res.redirect(`https://img.shields.io/badge/api-timeout-${red}.svg`);
-      else if (r.status) res.redirect(`https://img.shields.io/badge/api-${r.status}-${red}.svg`);
-      else res.redirect(`https://img.shields.io/badge/api-error-${red}.svg`);
+      if (res.timeout) display(res, `https://img.shields.io/badge/api-timeout-${red}.svg`);
+      else if (r.status) display(res, `https://img.shields.io/badge/api-${r.status}-${red}.svg`);
+      else display(res, `https://img.shields.io/badge/api-error-${red}.svg`);
     });
 
   return null;
 });
 
-app.listen(process.env.PORT || 80);
+app.listen(process.env.PORT || 8081);
 
 process.on('unhandledRejection', (err) => {
   throw err;
